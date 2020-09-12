8c9b1a6b08cf9ed38ae088b3148304b270036468
krishnayala <52299028+krishnayala@users.noreply.github.com>
2020-09-12 17:20:13 -0500

    For Testing

---
+++ Test.java
@@ -0,0 +1,15 @@
+package Practice;
+
+public class Publi {
+
+    private int eno = 1;
+    private String ename = "john";
+
+
+    void Employee(){
+
+
+        System.out.println(eno +" | "+ ename);
+    }
+
+}
